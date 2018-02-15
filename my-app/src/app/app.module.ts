7324ed63c70312c6f38193bf700a0313d5dfa2e4
huahang1 <huahangkidd@gmail.com>
2018-02-15 10:45:08 -0800

    apply the service for fetching data

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-// to make usage of ng-Model
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  //here declare the HeroesComponent so it could be used in app-component
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HeroService,
+    MessageService
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
