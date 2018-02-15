26402061f26d83f6d930164f694b59e791e14b77
huahang1 <huahangkidd@gmail.com>
2018-02-14 17:32:19 -0800

    create new module for display detail

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   //here declare the HeroesComponent so it could be used in app-component
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
